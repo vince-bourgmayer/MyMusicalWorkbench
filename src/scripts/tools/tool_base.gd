7fdd0788186ed2f3323f84342b352bc6b0461e85
vince-bourgmayer <v.bourgmayer@gmail.com>
2025-12-22 12:40:44 +0100

    feature: add 2 new scenes: tool_placeholder & tool_base with its scripts
    issue #3

---
+++ src/scripts/tools/tool_base.gd
@@ -0,0 +1,9 @@
+# -----------------------------------------------------------------------------
+# tool_base.gd
+# Copyright (c) 2025 Vincent Bourgmayer
+# License: MIT
+# -----------------------------------------------------------------------------
+extends StaticBody2D
+
+@export var tool_size := Vector2(32, 32)
+@export var tool_type := "generic"
