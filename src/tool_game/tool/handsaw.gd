8eda54a599728766d3f74a4d3126f6a8f76557d8
vince-bourgmayer <v.bourgmayer@gmail.com>
2025-12-25 02:13:42 +0100

    chore: refactor handsaw.gd, and minor cleaning in some other scritp: license, unused comment, etc.
    Issue #13

--- src/tool_game/tool/handsaw.gd
+++ src/tool_game/tool/handsaw.gd
@@ -1,3 +1,8 @@
+# -----------------------------------------------------------------------------
+# handsaw.gd
+# Copyright (c) 2025 Vincent Bourgmayer
+# License: MIT
+# -----------------------------------------------------------------------------
 extends Node2D
 
 @onready var visual = $visual
@@ -5,15 +10,15 @@ var visual_origin : Vector2
 var action := 0 # 0: idle, 1: push, 2: pull
 var action_distance := 0 # La distance parcouru par la scie > 0 : push distance, < 0 : pull distance
 const max_action_distance:= 150
-const distance_per_frame := 15
+const distance_per_frame := 15 # speed of the sawing movement
 
-# Called when the node enters the scene tree for the first time.
+const cut_increment := 0.1 # step of the cut progress
+
+
 func _ready() -> void:
 	print("Saw is ready")
 	visual_origin = visual.global_position # Store the origin position of the saw 
-	pass # Replace with function body.
-
-
+	#rotation = 45 # tourne la scie
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("push_saw"):
@@ -23,17 +28,16 @@ func _unhandled_input(event: InputEvent) -> void:
 	elif event.is_action_released("push_saw") || event.is_action_released("pull_saw"):
 		action = 0
 	pass
-	
 
+func make_progress():
+	position.y += cut_increment
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if action == 1 && action_distance < max_action_distance:
 		action_distance += distance_per_frame
-		print("I'm pushing the saw")
-		$visual.global_position.y -= distance_per_frame
+		$visual.position.y -= distance_per_frame
+		make_progress()
 	elif action == 2 && action_distance > max_action_distance*-1 :
 		action_distance -= distance_per_frame
-		print("I'm pulling the saw")
-		$visual.global_position.y += distance_per_frame		
-	pass
+		$visual.position.y += distance_per_frame
+		make_progress()
