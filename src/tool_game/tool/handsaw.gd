e6c7b97e3aa720d05e4d37dea1d4fbb25ba0d395
vince-bourgmayer <v.bourgmayer@gmail.com>
2025-12-25 12:22:15 +0100

    feat: Add guitar body wood blank sprite for handsawing game
    Texture has been handmade with Gimp.\nIssue #23

--- src/tool_game/tool/handsaw.gd
+++ src/tool_game/tool/handsaw.gd
@@ -9,7 +9,7 @@ extends Node2D
 var visual_origin : Vector2 
 var action := 0 # 0: idle, 1: push, 2: pull
 var action_distance := 0 # La distance parcouru par la scie > 0 : push distance, < 0 : pull distance
-const max_action_distance:= 150
+const sawblade_length:= 105 # depend on the sawblade's length
 const distance_per_frame := 15 # speed of the sawing movement
 
 const cut_increment := 0.1 # step of the cut progress
@@ -20,6 +20,7 @@ func _ready() -> void:
 	visual_origin = visual.global_position # Store the origin position of the saw 
 	#rotation = 45 # tourne la scie
 
+# Move below code to HandsawGame later
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("push_saw"):
 		action = 1
@@ -33,11 +34,24 @@ func make_progress():
 	position.y += cut_increment
 
 func _process(_delta: float) -> void:
-	if action == 1 && action_distance < max_action_distance:
-		action_distance += distance_per_frame
-		$visual.position.y -= distance_per_frame
+	if action == 1 && action_distance < sawblade_length:
+		push_saw()
 		make_progress()
-	elif action == 2 && action_distance > max_action_distance*-1 :
-		action_distance -= distance_per_frame
-		$visual.position.y += distance_per_frame
+	elif action == 2 && action_distance > sawblade_length*-1 :
+		pull_saw()
 		make_progress()
+		
+		
+func push_saw():
+	action_distance += distance_per_frame
+	$visual.position.y -= distance_per_frame
+	
+func pull_saw():
+	action_distance -= distance_per_frame
+	$visual.position.y += distance_per_frame
+	
+func plunge():
+	pass
+	
+func raise():
+	pass
