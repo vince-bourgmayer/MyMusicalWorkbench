62a46531030beed8fe8922d041adbf423c8f5218
vince-bourgmayer <v.bourgmayer@gmail.com>
2026-01-06 15:22:08 +0100

    feat: Add the cut waste polygon to the scene and apply the shader on it with new ShaderMaterial
    Issue #33

--- src/tool_game/handsaw_game/woodboard.gd
+++ src/tool_game/handsaw_game/woodboard.gd
@@ -7,6 +7,7 @@ extends Node2D
 
 @onready var remaining_wood_border : CollisionPolygon2D = $BorderArea2D/RemainingWoodBorder
 @onready var border_area2D := $BorderArea2D
+const INVISIBLE_CLOAK_MATERIAL = preload("res://src/shaders/materials/invisble_cloak_material.tres")
 
 var polygonSlicer : PolygonSlicer
 
@@ -22,7 +23,13 @@ func get_remaining_wood_border() -> Array[Segment]:
 		result.append(Segment.new(start, end))
 	return result
 
-func add_new_cut(startPoint: Vector2, endPoint: Vector2) -> PackedVector2Array:
+func add_new_cut(startPoint: Vector2, endPoint: Vector2) -> void:
 	var cut_result = polygonSlicer.cut_polygon(remaining_wood_border.polygon, startPoint, endPoint)
 	remaining_wood_border.polygon = cut_result[0]
-	return cut_result[1] #Returning waste polygon
+	hide_cut_waste(cut_result[1])
+	
+func hide_cut_waste(p_polygon: PackedVector2Array) -> void:
+	var poly = Polygon2D.new()
+	poly.polygon = p_polygon
+	poly.material = INVISIBLE_CLOAK_MATERIAL
+	$BodyBlank/InvisibleCloak.add_child(poly)
