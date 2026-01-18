51b047f96e73e395761e9fa4030394666a00d43e
vince-bourgmayer <v.bourgmayer@gmail.com>
2026-01-18 14:43:35 +0100

    feat: add Basic woodboard to HandPlaneGame
    Issue #45

---
+++ src/tool_game/handplane_game/woodboard_to_plane.gd
@@ -0,0 +1,15 @@
+# -----------------------------------------------------------------------------
+# woodboard_to_plane.gd
+# Copyright (c) 2026 Vincent Bourgmayer
+# License: MIT
+# -----------------------------------------------------------------------------
+extends Node2D
+class_name WoodBoardToPlane
+
+
+func _ready() -> void:
+	pass 
+
+
+func _process(_delta: float) -> void:
+	pass
