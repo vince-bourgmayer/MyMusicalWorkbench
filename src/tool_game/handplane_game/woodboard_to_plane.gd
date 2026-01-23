293ba56d0c9b6b4b6d9facfcdf09a1e9e865d216
vince-bourgmayer <v.bourgmayer@gmail.com>
2026-01-23 21:09:33 +0100

    chore: add thickness mask in woodboardToPlane
    Issue #45

--- src/tool_game/handplane_game/woodboard_to_plane.gd
+++ src/tool_game/handplane_game/woodboard_to_plane.gd
@@ -6,10 +6,27 @@
 extends Node2D
 class_name WoodBoardToPlane
 
+@onready var visual = $Visual
+var mask_thickess_image : Image
+var mask_thickness_texture : ImageTexture
 
 func _ready() -> void:
-	pass 
+	init_thickness_mask()
+	
+# mask that represent wood's thickness. White => highest depth, black: a hole
+func init_thickness_mask(): 
+	var size = visual.texture.get_size()
 
+	mask_thickess_image = Image.create(
+		size.x,
+		size.y,
+		false,
+		Image.FORMAT_L8
+	)
+
+	mask_thickess_image.fill(Color.WHITE)
+	mask_thickness_texture = ImageTexture.create_from_image(mask_thickess_image)
+	
 
 func _process(_delta: float) -> void:
 	pass
