e8b8edbe8c5f96ad422591b47751b656393f2a63
vince-bourgmayer <v.bourgmayer@gmail.com>
2026-09-09 00:16:03 +0200

    feat: implements jig class from Sprite2D

---
+++ src/tool_game/template_game/core/jig.gd
@@ -0,0 +1,27 @@
+# -----------------------------------------------------------------------------
+# jig.gd
+# Copyright (c) s2026 Vincent Bourgmayer
+# License: MIT
+# -----------------------------------------------------------------------------
+extends Sprite2D
+class_name Jig
+
+@export var move_speed: float = 200.0
+var movement_bounds: Rect2 = Rect2()
+var _current_direction := Vector2.ZERO
+
+func set_movement_bounds(bounds: Rect2) -> void:
+	movement_bounds = bounds
+
+func set_move_direction(direction: Vector2) -> void:
+	_current_direction = direction
+
+func _process(delta: float) -> void:
+	if _current_direction == Vector2.ZERO:
+		return
+
+	var new_position = position + _current_direction * move_speed * delta
+	if movement_bounds.has_area():
+		new_position.x = clamp(new_position.x, movement_bounds.position.x, movement_bounds.end.x)
+		new_position.y = clamp(new_position.y, movement_bounds.position.y, movement_bounds.end.y)
+	position = new_position
