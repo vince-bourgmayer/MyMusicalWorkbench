7baa454664c7aa2abf2ff352fb4f5fa1c0c6e6ae
vince-bourgmayer <v.bourgmayer@gmail.com>
2026-09-23 14:58:23 +0200

    chore: rename jig & pencil in templateGamemode, to follow snake_case
    issue #71

--- src/tool_game/template_game/template_game.gd
+++ src/tool_game/template_game/template_game.gd
@@ -21,10 +21,10 @@ var woodPiece : WoodPiece
 @onready var woodRenderer = $WoodRenderer
 @onready var _carousel : Carousel = $Carousel
 
-var pencil : Pencil
+var _pencil : Pencil
 var _pencil_scene: PackedScene = preload("res://src/tool_game/template_game/Pencil.tscn")
 var _jig_scene : PackedScene = preload("res://src/tool_game/template_game/Jig.tscn")
-var jig : Jig
+var _jig : Jig
 
 func _ready() -> void:
 	_current_state = 0
@@ -34,14 +34,13 @@ func _ready() -> void:
 	var woodPieceSize = Vector2(woodTexture.get_width(), woodTexture.get_height())
 	woodPiece = WoodPiece.new("Ash", woodPieceSize)
 	
-	
-	pencil = _pencil_scene.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE)
-	pencil.set_movement_bounds(self.get_viewport_rect())
-	pencil.drawing.connect(func(from, to):
+	_pencil = _pencil_scene.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE)
+	_pencil.set_movement_bounds(self.get_viewport_rect())
+	_pencil.drawing.connect(func(from, to):
 		woodPiece.apply(PencilOperation.new(woodRenderer.to_local(from), woodRenderer.to_local(to), 1))
 	)
-	
-	jig = _jig_scene.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE)
+
+	_jig = _jig_scene.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE)
 
 	_change_state(gameState.SELECT_JIG)
 	
@@ -52,38 +51,45 @@ func _ready() -> void:
 
 	woodRenderer.bind(woodPiece)
 
+func _exit_tree() -> void:
+	_jig.queue_free()
+	_pencil.queue_free()
+	_carousel.queue_free()
+	directionalInputReader.left_stick_direction_changed.disconnect(_on_left_stick_changed)
+	directionalInputReader.right_stick_direction_changed.disconnect(_on_right_stick_changed)
+
 func _on_left_stick_changed(direction: Vector2) -> void:
 	match _current_state:
 		gameState.SELECT_JIG:
 			_carousel.on_input_received(direction)
 		gameState.PLACE_JIG:
-			jig.set_move_direction(direction)
+			_jig.set_move_direction(direction)
 		gameState.DRAW_SHAPE:
-			jig.set_move_direction(direction)
+			_jig.set_move_direction(direction)
 
 func _on_right_stick_changed(direction: Vector2) -> void:
 	match _current_state:
 		gameState.SELECT_JIG:
 			pass
 		gameState.PLACE_JIG:
-			jig.set_rotation_direction(direction)
+			_jig.set_rotation_direction(direction)
 		gameState.DRAW_SHAPE:
-			pencil.set_move_direction(direction)
+			_pencil.set_move_direction(direction)
 
 func _enter_state(state: int) -> void:
 	match state:
 		gameState.SELECT_JIG:
 			self.add_child(_carousel)
 		gameState.PLACE_JIG:
-			if (not self.get_children().has(jig)):
-				jig.position = _carousel.currentSlot
-				self.add_child(jig)
-			jig.set_shape(_carousel.get_selected_path())
+			if (not self.get_children().has(_jig)):
+				_jig.position = _carousel.currentSlot
+				self.add_child(_jig)
+			_jig.set_shape(_carousel.get_selected_path())
 		gameState.DRAW_SHAPE:
-			if (not self.get_children().has(jig)):
-				self.add_child(jig)
-			triggersInputReader.right_trigger_pressure_changed.connect(pencil.set_drawing)
-			self.add_child(pencil)
+			if (not self.get_children().has(_jig)):
+				self.add_child(_jig)
+			triggersInputReader.right_trigger_pressure_changed.connect(_pencil.set_drawing)
+			self.add_child(_pencil)
 
 func _exit_state(state: int) -> void:
 	match state:
@@ -91,13 +97,13 @@ func _exit_state(state: int) -> void:
 			if (self.get_children().has(_carousel)):
 				self.remove_child(_carousel)
 		gameState.PLACE_JIG:
-			if (self.get_children().has(jig)):
-				self.remove_child(jig)
+			if (self.get_children().has(_jig)):
+				self.remove_child(_jig)
 		gameState.DRAW_SHAPE:
-			if (self.get_children().has(pencil)):
-				self.remove_child(pencil)
-			triggersInputReader.right_trigger_pressure_changed.disconnect(pencil.set_drawing)
-			
+			if (self.get_children().has(_pencil)):
+				self.remove_child(_pencil)
+			triggersInputReader.right_trigger_pressure_changed.disconnect(_pencil.set_drawing)
+
 
 func _on_game_cancelled() -> void:
 	cancel_game("Do you want to stop templating ?", "Press A to validate")
