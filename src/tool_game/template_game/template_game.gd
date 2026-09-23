c917c3fb9c1efa67a6947f818a9445742ad4bf0b
vince-bourgmayer <v.bourgmayer@gmail.com>
2026-09-23 12:30:12 +0200

    refactor: use parent class's methods (toolGame) for state changement
    issue #71

--- src/tool_game/template_game/template_game.gd
+++ src/tool_game/template_game/template_game.gd
@@ -13,7 +13,6 @@ const JIG_PATHS: Array[String] = [
 	"res://assets/shapes/strandberg_boden.png",
 ]
 
-var _currentState : gameState
 var directionalInputReader = DirectionalInputReader.new()
 var triggersInputReader = TriggersInputReader.new()
 
@@ -28,30 +27,33 @@ var _jig_scene : PackedScene = preload("res://src/tool_game/template_game/Jig.ts
 var jig : Jig
 
 func _ready() -> void:
+	_current_state = 0
+	_carousel.setup(JIG_PATHS)
+	
 	var woodTexture = woodRenderer.texture
 	var woodPieceSize = Vector2(woodTexture.get_width(), woodTexture.get_height())
 	woodPiece = WoodPiece.new("Ash", woodPieceSize)
+	
+	
 	pencil = _pencil_scene.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE)
 	pencil.set_movement_bounds(self.get_viewport_rect())
-	jig = _jig_scene.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE)
-
-	_set_select_jig_state()
-	directionalInputReader.left_stick_direction_changed.connect(_on_left_stick_changed)
-	directionalInputReader.right_stick_direction_changed.connect(_on_right_stick_changed)
 	pencil.drawing.connect(func(from, to):
 		woodPiece.apply(PencilOperation.new(woodRenderer.to_local(from), woodRenderer.to_local(to), 1))
 	)
+	
+	jig = _jig_scene.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE)
+
+	_change_state(gameState.SELECT_JIG)
+	
+	directionalInputReader.left_stick_direction_changed.connect(_on_left_stick_changed)
+	directionalInputReader.right_stick_direction_changed.connect(_on_right_stick_changed)
+	self.add_child(directionalInputReader)
+	self.add_child(triggersInputReader)
 
 	woodRenderer.bind(woodPiece)
 
-	self.add_child(directionalInputReader)
-	self.add_child(triggersInputReader)
-	
-	_carousel.setup(JIG_PATHS)
-
-
 func _on_left_stick_changed(direction: Vector2) -> void:
-	match _currentState:
+	match _current_state:
 		gameState.SELECT_JIG:
 			_carousel.on_input_received(direction)
 		gameState.PLACE_JIG:
@@ -60,59 +62,45 @@ func _on_left_stick_changed(direction: Vector2) -> void:
 			jig.set_move_direction(direction)
 
 func _on_right_stick_changed(direction: Vector2) -> void:
-	match _currentState:
+	match _current_state:
 		gameState.SELECT_JIG:
 			pass
 		gameState.PLACE_JIG:
 			jig.set_rotation_direction(direction)
 		gameState.DRAW_SHAPE:
 			pencil.set_move_direction(direction)
-			
-func handle_specific_input(_event: InputEvent) -> void:
-	match _currentState:
+
+func _enter_state(state: int) -> void:
+	match state:
 		gameState.SELECT_JIG:
-			handle_select_jig_input(_event)
+			self.add_child(_carousel)
 		gameState.PLACE_JIG:
-			handle_place_jig_input(_event)
+			if (not self.get_children().has(jig)):
+				jig.position = _carousel.currentSlot
+				self.add_child(jig)
+			jig.set_shape(_carousel.get_selected_path())
 		gameState.DRAW_SHAPE:
-			handle_draw_shape_input(_event)
-		_:
-			print("TemplateGame.handle_specific_input: Invalid gameState")
+			if (not self.get_children().has(jig)):
+				self.add_child(jig)
+			triggersInputReader.right_trigger_pressure_changed.connect(pencil.set_drawing)
+			self.add_child(pencil)
 
-func handle_select_jig_input(event: InputEvent) -> void:
-	if event.is_action_released("ui_accept"):
-		_set_place_jig_state()
-	elif event.is_action_released("ui_cancel"):
-		cancel_game("Do you want to stop templating ?", "Press A to validate")
-	
-func handle_place_jig_input(event: InputEvent) -> void:
-	if event.is_action_released("ui_accept"):
-		_set_draw_shape_state()
-	elif event.is_action_released("ui_cancel"):
-		self.remove_child(jig)
-		_set_select_jig_state()
+func _exit_state(state: int) -> void:
+	match state:
+		gameState.SELECT_JIG:
+			if (self.get_children().has(_carousel)):
+				self.remove_child(_carousel)
+		gameState.PLACE_JIG:
+			if (self.get_children().has(jig)):
+				self.remove_child(jig)
+		gameState.DRAW_SHAPE:
+			if (self.get_children().has(pencil)):
+				self.remove_child(pencil)
+			triggersInputReader.right_trigger_pressure_changed.disconnect(pencil.set_drawing)
+			
 
-func handle_draw_shape_input(event: InputEvent) -> void:
-	if event.is_action_released("ui_cancel"):
-		self.remove_child(pencil)
-		_set_place_jig_state()
+func _on_game_cancelled() -> void:
+	cancel_game("Do you want to stop templating ?", "Press A to validate")
 
-func _set_select_jig_state():
-	_currentState = gameState.SELECT_JIG
-	_carousel.visible = true
-
-func _set_place_jig_state():
-	_carousel.visible = false
-
-	if (!self.get_children().has(jig)):
-		jig.position = _carousel.currentSlot
-		self.add_child(jig)
-
-	jig.set_shape(_carousel.get_selected_path())
-	_currentState = gameState.PLACE_JIG
-
-func _set_draw_shape_state():
-	self.add_child(pencil)
-	triggersInputReader.right_trigger_pressure_changed.connect(pencil.set_drawing)
-
-	_currentState = gameState.DRAW_SHAPE
+func _get_final_state() -> int:
+	return gameState.DRAW_SHAPE
